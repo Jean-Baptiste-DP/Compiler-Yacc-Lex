05a99cd1672e93ffc827de819b1477c492cc82da
Jean-Baptiste-DP <jb.durandpierre@gmail.com>
2023-09-02 12:16:47 +0200

    allow cpp in project

--- test.bash
+++ test.bash
@@ -1,6 +1,42 @@
+#!/bin/bash
+
+# Directories
+src_dir="src"
+precompile_dir="output"
+
+length_src_dir=${#src_dir}
+
+# Yacc/Lex generation
 bison -d yaccLex/compiler.y
 flex yaccLex/compiler.l
-gcc -o compiler compiler.tab.c lex.yy.c src/*.c src/*/*.c
+
+# Yacc/Lex precompilation
+gcc -c -o compiler.tab.o compiler.tab.c 
+gcc -c -o lex.yy.o lex.yy.c
+
+# Src files.c precompilation
+find "$src_dir" -name '*.c' -type f | while read cfile; do
+    output_path="$precompile_dir${cfile:${length_src_dir}}"
+    mkdir -p "$(dirname "$output_path")"
+    gcc -c "$cfile" -o "${output_path%.c}.o"
+done
+
+# Src files.cpp precompilation
+find "$src_dir" -name '*.cpp' -type f | while read cfile; do
+    output_path="$precompile_dir${cfile:${length_src_dir}}"
+    mkdir -p "$(dirname "$output_path")"
+    g++ -c "$cfile" -o "${output_path%.c}.o"
+done
+
+# Compilation
+shopt -s globstar
+g++ -o compiler compiler.tab.o lex.yy.o $precompile_dir/**/*.o
+shopt -u globstar
+
+# Remove precompiled files
+rm -rf $precompile_dir
+rm lex.yy.o
+rm compiler.tab.o
 
 find test -type f -name "*.test.txt" | while read testFile; do
     outputFile=$(echo "$testFile"|sed 's/test.txt/output.txt/')
